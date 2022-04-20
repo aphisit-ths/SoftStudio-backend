ba2efebd249f9a9c97d6940536caea4853b23f5d
Oath <82308226+apisit-kmitl@users.noreply.github.com>
2022-04-20 20:07:14 +0700

    add new ignore

---
+++ dhamma/Models/Comment.cs
@@ -0,0 +1,9 @@
+namespace dhamma.Models;
+public class Comment
+{
+    public int CommentId { get; set; }
+    public string? Username { get; set; }
+    public string? Description { get; set; }
+    public string? Date { get; set; }
+    public List<Like>? LikeList {get;set;}
+}
